3df5f8c60dbb7b406748ba2318d21a54fce203c1
musaakyil1 <msaakyil21@gmail.com>
2025-04-04 15:30:41 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector("#menu-btn");
+
+
+
+
+
+menuBtn.addEventListener('click',() =>{
+    navbar.classList.toggle("active");
+});
